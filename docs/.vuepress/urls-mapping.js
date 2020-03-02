3c5ec1d1e6ce13f5286d23d612b87ccb9bb4894d
Inessa <inessa@Inessas-MacBook-Air.local>
2020-03-02 14:37:42 +0300

    improvements

--- docs/.vuepress/urls-mapping.js
+++ docs/.vuepress/urls-mapping.js
@@ -163,6 +163,6 @@ module.exports = {
   "/ru/stand_alone/#extended-integration-conf-example-for-reference": "/ru/stand_alone/#integration-config-file",
   "/stand_alone/#further-steps": "/stand_alone/#imunify360-stand-alone",
   "/ru/stand_alone/#further-steps": "/ru/stand_alone/#imunify360-stand-alone",
-  "/ru/features/smtp-traffic-manager": "/ru/features/#smtp-traffic-manager",
-  "/features/smtp-traffic-manager": "/features/#smtp-traffic-manager"
+  "/ru/features/smtp-traffic-manager/": "/ru/features/#smtp-traffic-manager",
+  "/features/smtp-traffic-manager/": "/features/#smtp-traffic-manager"
 };
